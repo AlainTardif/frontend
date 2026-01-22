a54cdb0ea443dd06afa5e5e9044a9eed4324d082
Alain Tardif <tardif.alain@gmail.com>
2026-01-22 14:33:29 +0100

    feat: add POST method to HttpClient with test

--- src/core/http/http-client.js
+++ src/core/http/http-client.js
@@ -19,7 +19,36 @@ export class HttpClient {
             throw error
         }
         
-        return response.json() // Si tout est bon on retourne le JSON
+        return response.json()
+    }
+
+    /**
+     * Send a POST request to the endpoint
+     * @param {string} endpoint
+     * @param {object} data
+     * @returns Promise<any>
+     * @raise Error if status <> 20x ou 30x
+     */
+    async post(endpoint, data) {
+        const response = await fetch(
+            `${this.#api}${endpoint}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }
+        )
+        
+        if (!response.ok) {
+            const error = new Error(`HTTP status ${response.status} : ${response.statusText}`)
+            error.status = response.status
+            error.response = response
+            throw error
+        }
+        
+        return response.json()
     }
 }
 
@@ -31,10 +60,19 @@ const consumer = async () => {
         const payload = await httpClient.get('users')
         console.table(payload)
     } catch (error) {
-        console.error(`${error.status} - ${JSON.stringify(error.response)}`)
-        console.error(`Erreur: ${error.message}`)
+        console.error(`${error.status} : ${error.message}`)
     }
 }
 
-// Call the function
+const add = async () => {
+    const newProduct = {
+        name: "Product 2"
+    }
+    
+    const productCreated = await httpClient.post('users', newProduct)
+    console.log(productCreated)
+}
+
+// Call the functions
+add()
 consumer()
