5d2b22e14c1ae3e2c41a46a79abeb164ca91f7ca
Alain Tardif <tardif.alain@gmail.com>
2026-01-22 11:15:39 +0100

    feat: add HttpClient with GET request promise

--- src/core/http/http-client.js
+++ src/core/http/http-client.js
@@ -1,8 +1,15 @@
 export class HttpClient {
-    #api = 'http:localhost:3000'; //Private attribute 
+    #api = 'http://localhost:3000'; //Private attribute
+    
+   /**
+   * Send a GET request to the endpoint
+   * @param {string} endpoint
+   * @returns JSON resource (payload)
+   */
 
     get(endpoint) {
-        return fetch(`${this.#api}/${endpoint}`)
+        return fetch(
+            `${this.#api}/${endpoint}`)
             .then(response => response.json());
     }
 }
