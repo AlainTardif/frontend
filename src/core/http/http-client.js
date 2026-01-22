6b1e17239459f23ccf3d48208651c174b575da80
Alain Tardif <tardif.alain@gmail.com>
2026-01-22 11:42:38 +0100

    feat: add HttpClient with working API connection

--- src/core/http/http-client.js
+++ src/core/http/http-client.js
@@ -1,15 +1,27 @@
 export class HttpClient {
-    #api = 'http://localhost:3000'; //Private attribute
+    #api = 'http://localhost:3000/'; //Private attribute
     
    /**
    * Send a GET request to the endpoint
    * @param {string} endpoint
-   * @returns JSON resource (payload)
+   * @returns Promise<Response>
    */
 
     get(endpoint) {
         return fetch(
-            `${this.#api}/${endpoint}`)
-            .then(response => response.json());
+            `${this.#api}${endpoint}`
+        )
     }
+} 
+
+// Essayer de communiquer avec le backend
+const httpClient = new HttpClient()
+
+const consumer = async () => {
+     const response = await httpClient.get('users')
+     console.log(`Response status: ${response.status}` )
+     
 }
+
+// Call the function
+consumer()
