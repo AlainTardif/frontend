b916478fbbd6b5865537eaec73bcf3d1ac1635c8
Alain Tardif <tardif.alain@gmail.com>
2026-01-22 13:52:25 +0100

    feat: add error handling with try/catch in HttpClient

--- src/core/http/http-client.js
+++ src/core/http/http-client.js
@@ -1,28 +1,39 @@
 export class HttpClient {
-    #api = 'http://localhost:3000/'; //Private attribute
+    #api = 'http://localhost:3000/';
     
-   /**
-   * Send a GET request to the endpoint
-   * @param {string} endpoint
-   * @returns Promise<Response>
-   */
-
-    get(endpoint) {
-        return fetch(
+    /**
+     * Send a GET request to the endpoint
+     * @param {string} endpoint
+     * @returns Promise<any>
+     * @raise Error if status <> 20x ou 30x
+     */
+    async get(endpoint) {
+        const response = await fetch(
             `${this.#api}${endpoint}`
         )
+        
+        if (!response.ok) {
+            const error = new Error(`HTTP status ${response.status} : ${response.statusText}`)
+            error.status = response.status
+            error.response = response
+            throw error
+        }
+        
+        return response.json()
     }
-} 
+}
 
 // Essayer de communiquer avec le backend
 const httpClient = new HttpClient()
 
 const consumer = async () => {
-     const response = await httpClient.get('users')
-     console.log(`Response status: ${response.status}` )
-     const payload = await response.json()
-     console.table(payload)
-     
+    try {
+        const payload = await httpClient.get('users')
+        console.table(payload)
+    } catch (error) {
+        console.error(`Erreur: ${error.message}`)
+        console.error(`Status: ${error.status}`)
+    }
 }
 
 // Call the function
